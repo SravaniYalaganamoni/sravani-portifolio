a41d2fc7d570a8c09fe4f7aed032b478cc915566
Sravani Yalaganamoni <ysravaniyadav2@gmail.com>
2026-06-21 01:19:13 -0700

    Remove resume link from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -70,12 +70,6 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link as={Link} to="/resume" onClick={() => updateExpanded(false)}>
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
             {/* New Contact Section */}
             <Nav.Item>
               <Nav.Link as={Link} to="/contact" onClick={() => updateExpanded(false)}>
